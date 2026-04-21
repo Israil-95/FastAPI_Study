1a13f444053d96cb8b7d618dd2270b859dfa7ac5
Israil-95 <israil.vagapov95@gmail.com>
2026-04-22 02:16:59 +0300

    Add docker configuration

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:2008@localhost:3306/test_db"
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/my_database"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
